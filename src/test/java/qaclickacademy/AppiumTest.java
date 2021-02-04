be5a7ae30db5fcc8f700751c67a1b4b9f5290050
renuka37 <renuka.nandyala539@gmail.com>
2021-02-04 17:48:51 +0100

    first commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeappAndhroid()
+	{
+		System.out.println("NativeappAndhroid");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void PostJira10()
+	{
+		System.out.println("PostJira10");
+	}
+	@Test
+	public void deletetwitter10()
+	{
+		System.out.println("deletetwitter10");
+	}
+
+}
